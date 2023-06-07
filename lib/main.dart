ad01a8cafa7489f4d69e6a57ed0bc76bfc013208
retail-oukaku <112593821+retail-oukaku@users.noreply.github.com>
2023-06-07 10:18:20 +0900

    課題1.ボタンを押すとカウントアップされるシンプルなカウンターアプリを作成する (#14)
    * Kadai1クラスを追加
    
    * 課題１対応
    
    * change test code
    
    * change test code
    
    * if文をsetStateの外に
    
    * "->'
    シングルクォーテーションに変更
    
    * change file's name
    
    * 静的解析エラー、Warningなどの対応
    
    * space formatの修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'kadai1/count_up_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter layout demo',
-      home: Home(),
+      home: const Home(),
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -22,6 +23,8 @@ class MyApp extends StatelessWidget {
 
 // 画面遷移をする部分のコード
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,11 +33,11 @@ class Home extends StatelessWidget {
       ),
       body: ListView(
         children:[
-          _menuItem("課題１", const Icon(Icons.settings),context),
-          _menuItem("課題2", const Icon(Icons.map),context),
-          _menuItem("課題3", const Icon(Icons.room),context),
-          _menuItem("課題4", const Icon(Icons.local_shipping),context),
-          _menuItem("課題5", const Icon(Icons.airplanemode_active),context),
+          _menuItem('課題1', const Icon(Icons.settings),context),
+          _menuItem('課題2', const Icon(Icons.map),context),
+          _menuItem('課題3', const Icon(Icons.room),context),
+          _menuItem('課題4', const Icon(Icons.local_shipping),context),
+          _menuItem('課題5', const Icon(Icons.airplanemode_active),context),
         ],
       ),
     );
@@ -42,31 +45,36 @@ class Home extends StatelessWidget {
   Widget _menuItem(String title, Icon icon,BuildContext context) {
     return GestureDetector(
       child: Container(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8),
         decoration: const BoxDecoration(
-            border: Border(bottom: BorderSide(width: 1.0,color: Colors.grey))
+            border: Border(bottom: BorderSide(color: Colors.grey)),
         ),
         child: Row (
           children: [
             Container(
-              margin: EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10),
               child: icon,
             ),
             Text(
               title,
               style: const TextStyle(
                   color: Colors.black,
-                  fontSize: 18.0
+                  fontSize: 18,
               ),
             ),
           ],
         ),
       ),
-      onTap: () {
-        if (title == "課題１") {
-
+      onTap: () async {
+        if (title == '課題1') {
+          await Navigator
+              .of(context)
+              .push(MaterialPageRoute<dynamic>(builder: (BuildContext context) {
+                return const CountUpPage();
+                },
+          ),
+          );
         }
-        print(title);
       },
     );
   }
