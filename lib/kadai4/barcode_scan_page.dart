800b66a8f91f28990f667451f3b3bdc9e5591db7
retail-oukaku <ou_kaku@retail-ai.jp>
2023-06-12 16:35:08 +0900

    add oss(flutter_barcode_scanner)

--- lib/kadai4/barcode_scan_page.dart
+++ lib/kadai4/barcode_scan_page.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_web_test_project/kadai4/product_detail_page.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class BarcodeScanPage extends StatefulWidget {
   const BarcodeScanPage({super.key});
@@ -10,46 +12,113 @@ class BarcodeScanPage extends StatefulWidget {
 }
 
 class _BarcodeScanPageState extends State<BarcodeScanPage> {
-  void _incrementCounter() {
-    Navigator.of(context).push(MaterialPageRoute<dynamic>(
-      builder: (BuildContext context) {
-        return const ProductDetailsPage();
-        },
-    ),);
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
   }
+
+  Future<void> startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+        '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
+        .listen((barcode) => print(barcode));
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      debugPrint(barcodeScanRes);
+      _skipToProductDetail(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => {
-              Navigator.of(context).pop('Back')
-            },
-          ),
-          title: const Text('課題４：バーコードスキャン'),
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Barcode scan')),
+            body: Builder(
+              builder: (BuildContext context) {
+                return SafeArea(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Flex(
+                        direction: Axis.vertical,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          ElevatedButton(
+                              onPressed: scanBarcodeNormal,
+                              child: const Text('Start barcode scan'),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          // ElevatedButton(
+                          //     onPressed: scanQR,
+                          //     child: const Text('Start QR scan')),
+                          // ElevatedButton(
+                          //     onPressed: startBarcodeScanStream,
+                          //     child: const Text('Start barcode scan stream')),
+                          Text('Scan result : $_scanBarcode\n',
+                              style: const TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                );
+              },
+            ),
         ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
+    );
+  }
+
+  void _skipToProductDetail(String barcode){
+    Navigator.of(context).push(
+      MaterialPageRoute<dynamic>(
+        builder: (BuildContext context) {
+          return const ProductDetailsPage();
+        },
       ),
     );
   }
